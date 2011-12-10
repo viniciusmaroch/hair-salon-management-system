1ee6cf7a80432923f27037bd23e3b19d6bebc3d7
albert.saave@gmail.com <albert.saave@gmail.com@5c5358a5-6cb8-46af-af63-b44c6816e18e>
2011-12-10 04:27:56 +0000

    Extracted sql creation of the hsms_db schema and sql creation of services table from MysqSQLTests.sql into their respective files.
    First run the schema creation script and then the service table.

---
+++ trunk/HSMS/bootstrap/schema_creation.sql
@@ -0,0 +1,3 @@
+-- Creates SQL Schema hsms_db
+CREATE SCHEMA IF NOT EXISTS `hsms_db` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci ;
+USE `hsms_db` ;
---
+++ trunk/HSMS/bootstrap/service_table.sql
@@ -0,0 +1,12 @@
+-- -----------------------------------------------------
+-- Table `hsms_db`.`services`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `hsms_db`.`services` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `description` text,
+  `duration` time DEFAULT NULL,
+  `cost` float DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1$$

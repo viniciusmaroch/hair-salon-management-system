3cb0aa0c982a5cf585cd48cdf0ae72a2e6796e8b
w1g2h300@gmail.com <w1g2h300@gmail.com@5c5358a5-6cb8-46af-af63-b44c6816e18e>
2011-12-15 23:51:32 +0000

    news model

---
+++ HSMS/p/app/models/news.php
@@ -0,0 +1,35 @@
+<?php
+class News extends AppModel {
+	var $name = 'News';
+	var $primaryKey = 'NID';
+	var $validate = array(
+		'NID' => array(
+			'numeric' => array(
+				'rule' => array('numeric'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+			'notempty' => array(
+				'rule' => array('notempty'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'UID' => array(
+			'numeric' => array(
+				'rule' => array('numeric'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+	);
+}

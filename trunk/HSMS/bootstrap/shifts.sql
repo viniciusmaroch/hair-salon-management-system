ab7a130b926741bb88c1b22748873c49a2b0d0bb
w1g2h300@gmail.com <w1g2h300@gmail.com@5c5358a5-6cb8-46af-af63-b44c6816e18e>
2011-12-18 09:30:40 +0000

    shifts.sql

---
+++ trunk/HSMS/bootstrap/shifts.sql
@@ -0,0 +1,18 @@
+--
+-- Database: `hsms_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shifts`
+--
+
+CREATE TABLE IF NOT EXISTS `hsms_db`.`shifts` (
+  `SHID` int(15) NOT NULL AUTO_INCREMENT,
+  `UID` int(15) NOT NULL,
+  `from` time NOT NULL,
+  `to` time NOT NULL,
+  PRIMARY KEY (`SHID`),
+  KEY `UID` (`UID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;

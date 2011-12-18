f6b5f8a4556ff5289ee7bafe0dd10c43d93929f1
w1g2h300@gmail.com <w1g2h300@gmail.com@5c5358a5-6cb8-46af-af63-b44c6816e18e>
2011-12-18 09:31:56 +0000

    shifts controller

---
+++ HSMS/p/app/controllers/shifts_controller.php
@@ -0,0 +1,61 @@
+<?php
+class ShiftsController extends AppController {
+
+	var $name = 'Shifts';
+
+	function index() {
+		$this->Shift->recursive = 0;
+		$this->set('shifts', $this->paginate());
+	}
+
+	function view($id = null) {
+		if (!$id) {
+			$this->Session->setFlash(__('Invalid shift', true));
+			$this->redirect(array('action' => 'index'));
+		}
+		$this->set('shift', $this->Shift->read(null, $id));
+	}
+
+	function add() {
+		if (!empty($this->data)) {
+			$this->Shift->create();
+			if ($this->Shift->save($this->data)) {
+				$this->Session->setFlash(__('The shift has been saved', true));
+				$this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash(__('The shift could not be saved. Please, try again.', true));
+			}
+		}
+	}
+
+	function edit($id = null) {
+		if (!$id && empty($this->data)) {
+			$this->Session->setFlash(__('Invalid shift', true));
+			$this->redirect(array('action' => 'index'));
+		}
+		if (!empty($this->data)) {
+			if ($this->Shift->save($this->data)) {
+				$this->Session->setFlash(__('The shift has been saved', true));
+				$this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash(__('The shift could not be saved. Please, try again.', true));
+			}
+		}
+		if (empty($this->data)) {
+			$this->data = $this->Shift->read(null, $id);
+		}
+	}
+
+	function delete($id = null) {
+		if (!$id) {
+			$this->Session->setFlash(__('Invalid id for shift', true));
+			$this->redirect(array('action'=>'index'));
+		}
+		if ($this->Shift->delete($id)) {
+			$this->Session->setFlash(__('Shift deleted', true));
+			$this->redirect(array('action'=>'index'));
+		}
+		$this->Session->setFlash(__('Shift was not deleted', true));
+		$this->redirect(array('action' => 'index'));
+	}
+}

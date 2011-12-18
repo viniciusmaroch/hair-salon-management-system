6dcb15cb2b8a52c70f503527e9534cee31d6abfa
w1g2h300@gmail.com <w1g2h300@gmail.com@5c5358a5-6cb8-46af-af63-b44c6816e18e>
2011-12-18 09:31:36 +0000

    shift model

---
+++ trunk/HSMS/p/app/models/shift.php
@@ -0,0 +1,65 @@
+<?php
+class Shift extends AppModel {
+	var $name = 'Shift';
+	var $primaryKey = 'SHID';
+	var $displayField = 'SHID';	
+	var $validate = array(
+		'SHID' => array(
+			'numeric' => array(
+				'rule' => array('numeric'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+			'notempty' => array(
+				'rule' => array('notempty'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'UID' => array(
+			'numeric' => array(
+				'rule' => array('numeric'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'from' => array(
+			'time' => array(
+				'rule' => array('time'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'to' => array(
+			'time' => array(
+				'rule' => array('time'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+	);
+	var $belongsTo = array(
+					'User' => array(
+						'className' => 'User',
+						'foreignKey' => 'UID',
+						'conditions' => '',
+						'fields' => '',
+						'order' => ''
+	)
+	);
+}

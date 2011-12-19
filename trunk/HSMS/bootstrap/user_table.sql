70ae1a18d8443cf733173838d4ebc612a090d3fa
w1g2h300@gmail.com <w1g2h300@gmail.com@5c5358a5-6cb8-46af-af63-b44c6816e18e>
2011-12-19 10:04:39 +0000

    User + Person

--- trunk/HSMS/bootstrap/user_table.sql
+++ trunk/HSMS/bootstrap/user_table.sql
@@ -8,6 +8,15 @@ CREATE  TABLE IF NOT EXISTS `hsms_db`.`users` (
   `last_login` DATETIME  ,
   `is_employee` TINYINT(1)  DEFAULT false ,
   `is_manager` TINYINT(1)  DEFAULT false ,
-  PRIMARY KEY (`id`),
+  `firstName` varchar(25) NOT NULL,
+  `lastName` varchar(25) NOT NULL,
+  `DOB` datetime NOT NULL,
+  `gender` varchar(6) NOT NULL,
+  `phoneNumber` varchar(20) NOT NULL,
+  `street` varchar(50) NOT NULL,
+  `postalCode` varchar(15) NOT NULL,
+  `city` varchar(40) NOT NULL,
+  `country` varchar(40) NOT NULL,
+  PRIMARY KEY (`UID`),
   UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1$$
